7f0c8099892a3bc39580341f232f8417fc03569c
htruchla <hana_truchla@hotmail.com>
2026-02-23 13:39:48 -0500

    remvoing pyvista from install script

---
+++ examples/cfd/external_aerodynamics/xaeronet/surface_rorqual/01_preprocess_nopyvista.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#SBATCH --job-name=xaeronet_preprocess
+#SBATCH --output=logs/xaeronet_preprocess.out
+#SBATCH --error=logs/xaeronet_preprocess.err
+#SBATCH --time=12:00:00
+#SBATCH --mem=128G
+#SBATCH --cpus-per-task=32        # Should match cfg.num_preprocess_workers in conf/config.yaml
+
+#SBATCH --account=rrg-nadaraja-ac
+#SBATCH --mail-user=hana.truchla@mail.mcgill.ca
+#SBATCH --mail-type=ALL
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+
+echo " ***********Loading modules************** "
+
+# ---------- Environment ----------
+module --force purge
+echo " ***********Loading StdEnv/2023 at time $(date)************** "
+module load StdEnv/2023
+echo " ***********Loading python/3.11.5 at time $(date)************** "
+module load python/3.11.5
+echo " ***********Loading cuda/12.6 at time $(date)************** "
+module load cuda/12.6
+echo " ***********Loading vtk/9.3.0 at time $(date)************** "
+module load vtk/9.3.0
+echo " ********Modules loaded exporting cuda directory  ********"
+export CUDA_HOME=$(dirname $(dirname $(which nvcc)))
+
+
+echo " **************Setting path for env ***************"
+VENV_PATH="$HOME/envs/xaeronet"
+source "$VENV_PATH/bin/activate"
+
+
+# ---------- Setup ----------
+cd $SLURM_SUBMIT_DIR
+mkdir -p logs partitions point_clouds
+
+# ---------- Run ----------
+echo "=== [$(date)] Preprocessing started on $(hostname) ==="
+python preprocessor_rorqual.py
+EXIT_CODE=$?
+echo "=== [$(date)] Preprocessing finished with exit code $EXIT_CODE ==="
+exit $EXIT_CODE
